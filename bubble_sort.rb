a2dc3b6cadf8ca884425b20483e4a99e3c5ebc54
abdo20 <abdelrhmanadel93@gmail.com>
2017-05-26 19:18:57 +0200

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,3 +18,21 @@ end
 
 p main([4,3,78,2,0,2])
 
+
+def bubble_sort_by(arr)
+  sorted=false           # assume the list is unsorted
+  unorder_items_length=arr.length       # assume their's no item in the list in the right index
+  while !sorted          # check if the list is sorted
+    sorted=true          # assume the list will be sorted in this loop
+    (0...unorder_items_length-1).each do |index|      # iterate through every index expect the last
+      if yield(arr[index],arr[index+1]) >= 1 
+        sorted=false      #if the if statement occur then the list still need to go through the loop again
+        arr[index], arr[index+1]=arr[index+1], arr[index]  
+      end
+    end
+    unorder_items_length-=1       #the last item will be certainly in the right index each loop
+  end
+  return arr
+end
+
+p bubble_sort_by(["hi","hello","hey"]) { |l, r| l.length - r.length }
